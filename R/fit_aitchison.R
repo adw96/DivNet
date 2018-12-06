6e15eb3f913c330a4e9be6d8f4399d1aed4ecb17
Bryan Martin <bmartin6@uw.edu>
2018-12-05 17:27:50 -0800

    Fix OLS with X = 0 covariates. Fixes #9

--- R/fit_aitchison.R
+++ R/fit_aitchison.R
@@ -43,6 +43,8 @@ fit_aitchison <- function(W,
   output_list$X <- X
   no_covariates <- ncol(X) == 0
   
+  X_c <- scale(X, scale = FALSE)
+  
   # base taxon
   if (is.null(base)) { 
     base <- pick_base(W)
@@ -56,8 +58,8 @@ fit_aitchison <- function(W,
   b0 <- colMeans(Y_p)
   eY <- tcrossprod(rep(1, N), b0) 
   if (!no_covariates)  {
-    b <- OLS(X, Y_p)
-    eY <- eY + X %*% b
+    b <- OLS(X_c, Y_p)
+    eY <- eY + X_c %*% b
   }
   sigma <- var(Y_p - eY) # (Q-1) x (Q-1)
   
@@ -148,8 +150,8 @@ fit_aitchison <- function(W,
     # update b
     eY <- tcrossprod(rep(1, N), b0) 
     if (!no_covariates)  {
-      b <- OLS(X, Y_new)
-      eY <- eY + X %*% b
+      b <- OLS(X_c, Y_new)
+      eY <- eY + X_c %*% b
     }
     
     ### STORE after updating
@@ -188,7 +190,7 @@ fit_aitchison <- function(W,
     b_EM <- apply(b_list_reduced, c(1, 2), mean)
     output_list$beta <- b_EM
     
-    fitted_y <- X %*% b_EM + matrix(b0_EM, ncol = Q-1, nrow = N, byrow=T)
+    fitted_y <- X_c %*% b_EM + matrix(b0_EM, ncol = Q-1, nrow = N, byrow=T)
     
   }
   
---
+++ tests/testthat/test_zeros.R
@@ -0,0 +1,57 @@
+library(DivNet)
+library(magrittr)
+context("Test zeroes")
+
+set.seed(1)
+n <- 6
+n_taxa <- 6
+my_counts <- matrix(c(rep(c(rep(100, n_taxa/2), rep(0, n_taxa/2)), n/2),
+                      rep(c(rep(0, n_taxa/2), rep(100, n_taxa/2)), n/2)),
+                    nrow = n, ncol = n_taxa, byrow=T)
+my_counts
+my_discrete_covariate <- cbind(1, rep(c(0,1), each = n/2))
+
+
+test_that("DivNet estimates are basically correct", {
+  
+  dv <- divnet(my_counts[1:3, ], X=my_discrete_covariate[1:3, ], base = 1)
+  estimates <- dv$shannon %>% summary %$% estimate 
+  expect_true(max(estimates) == min(estimates))
+  expect_equal(max(estimates), 
+               shannon_true(c(1/3, 1/3, 1/3)), tolerance = 1e-2)
+  
+  my_counts_2 <- my_counts
+  my_counts_2[1:3, ] <- 100
+  my_counts_2
+  dv2 <- divnet(my_counts_2, X=my_discrete_covariate, base = 1)
+  estimates2 <- dv2$shannon %>% summary %$% estimate 
+  expect_equal(estimates2[6], estimates[1], tolerance = 1e-2)
+  expect_equal(estimates2[1], 
+               shannon_true(rep(1/6, 6)), tolerance = 1e-2)
+  expect_equal(estimates2[6], 
+               shannon_true(rep(1/3, 3)), tolerance = 1e-2)
+  
+})
+
+
+
+test_that("Identical observations give identical estimates", {
+  
+  dv <- divnet(my_counts, X=my_discrete_covariate, base = 1)
+  
+  estimates <- dv$shannon %>% summary %$% estimate 
+  expect_lt(max(estimates) - min(estimates), 0.01)
+  
+  W <- matrix(pmax(1, my_counts), nrow = 6)
+  X = my_discrete_covariate
+  tuning = NULL
+  perturbation = NULL 
+  network = NULL
+  base = 1
+  ncores = NULL
+  
+  fa <- fit_aitchison(W, X=my_discrete_covariate, base = 6)
+  expect_equal(fa$fitted_z[1,1], 1/3, tolerance = 1e-2)
+  expect_equal(fa$fitted_z[6,6], 1/3, tolerance = 1e-2)
+  
+})
