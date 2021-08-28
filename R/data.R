ba59e2d2b0d89acb5fdfd33b93e9f11848ca9249
ailurophilia <dsc24@uw.edu>
2021-08-27 17:56:22 -0700

    added documentation for Sinha et al (2017) data

--- R/data.R
+++ R/data.R
@@ -1,10 +1,24 @@
-#' phyloseq object from Lee et al (2015) 
+#' phyloseq object from Lee et al (2015)
 #'
-#' A dataset containing the abundance table, covariate information, and taxonomy of a 16S study of basalts on the Dorado Outcrop 
+#' A dataset containing the abundance table, covariate information, and taxonomy of a 16S study of basalts on the Dorado Outcrop
 #'
-#' @format A phyloseq object containing the data from Lee et al (2015) 
+#' @format A phyloseq object containing the data from Lee et al (2015)
 #'
-#' 
+#'
 #' @source \url{https://astrobiomike.github.io/amplicon/workflow_ex}
 #' @references "Microbial Communities on Seafloor Basalts at Dorado Outcrop Reflect Level of Alteration and Highlight Global Lithic Clades" by Lee et. al (2015), Frontiers in Microbiology
 "Lee"
+
+#' OTU data from Sinha et al. (2017)
+#'
+#' A dataset containing OTU counts reported by Sinha et al. (2017)
+#'  in colorectal cancer patients and controls
+#' as sequenced by sequencing laboratory B and processed by bioinformatics
+#' laboratory 2.
+#'
+#' @format A dataframe containing a subset of data from Sinha et al. (2017)
+#'
+#'
+#' @source \url{https://www.nature.com/articles/nbt.3981}
+#' @references "Sinha, Rashmi, et al. "Assessment of variation in microbial community amplicon sequencing by the Microbiome Quality Control (MBQC) project consortium." Nature biotechnology 35.11 (2017): 1077-1086.
+"mbqc_B2_crc"
