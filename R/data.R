c6f22ba5a10e1079ef81338a935ef48dbb509180
ailurophilia <dsc24@uw.edu>
2021-09-02 12:52:12 -0700

    Sinha et al. data also now saved as a phyloseq object in data. Updates to beta_diversity and centroids vignettes (removed dependency on devtools, corrected formatting in header, updated data() calls to reflect changes in what data is loaded with DivNet)

--- R/data.R
+++ R/data.R
@@ -9,6 +9,7 @@
 #' @references "Microbial Communities on Seafloor Basalts at Dorado Outcrop Reflect Level of Alteration and Highlight Global Lithic Clades" by Lee et. al (2015), Frontiers in Microbiology
 "Lee"
 
+
 #' OTU data from Sinha et al. (2017)
 #'
 #' A dataset containing OTU counts reported by Sinha et al. (2017)
@@ -22,3 +23,18 @@
 #' @source \url{https://www.nature.com/articles/nbt.3981}
 #' @references "Sinha, Rashmi, et al. "Assessment of variation in microbial community amplicon sequencing by the Microbiome Quality Control (MBQC) project consortium." Nature biotechnology 35.11 (2017): 1077-1086.
 "B2"
+
+
+#' Species data from Sinha et al. (2017) in phyloseq format
+#'
+#' A dataset containing OTU counts reported by Sinha et al. (2017)
+#'  in colorectal cancer patients and controls
+#' as sequenced by sequencing laboratory B and processed by bioinformatics
+#' laboratory 2.
+#'
+#' @format A phyloseq object containing a subset of data from Sinha et al. (2017)
+#'
+#'
+#' @source \url{https://www.nature.com/articles/nbt.3981}
+#' @references "Sinha, Rashmi, et al. "Assessment of variation in microbial community amplicon sequencing by the Microbiome Quality Control (MBQC) project consortium." Nature biotechnology 35.11 (2017): 1077-1086.
+"B2_phyloseq"
--- vignettes/README.md
+++ vignettes/README.md
@@ -1,3 +1,5 @@
-Currently we have a detailed vignettes about DivNet and how to use it on phyloseq objects. We plan to make more vignettes as we better understand what users of DivNet are likely to need. Feel free to log a feature request under Issues if there is something you would like to see but don't.
+Currently we have a detailed vignettes about DivNet and how to use it on phyloseq objects. In addition, we have vignettes covering using DivNet for beta diversity analysis as well as a more conceptual vignette outlining the connection between beta diversity analyses and centroid estimation.
+
+We plan to make more vignettes as we better understand what users of DivNet are likely to need. Feel free to log a feature request under Issues if there is something you would like to see but don't.
 
 [Here](https://github.com/adw96/stamps2018/blob/master/estimation/diversity-lab.R) you will find an interactive tutorial on diversity estimation using [breakaway](https://github.com/adw96/breakaway) and [DivNet](https://github.com/adw96/DivNet).
